2da8b70a5c451f101a6633442ed205992db9dfc2
namazu-san <vppayassistenza@gmail.com>
2024-08-05 15:41:50 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,44 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invio Email</title>
 </head>
 <body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recupera i dati dal modulo
+        $nome = htmlspecialchars($_POST['nome']);
+        $cognome = htmlspecialchars($_POST['cognome']);
+        $email = htmlspecialchars($_POST['email']);
+        $telefono = htmlspecialchars($_POST['telefono']);
+        $messaggio = htmlspecialchars($_POST['messaggio']);
 
+        // Imposta gli header dell'email
+        $headers = "From: info@finvideoholding.it\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Raccogli i dati del form e sanitizza l'input
-    $nome = htmlspecialchars($_POST['nome']);
-    $cognome = htmlspecialchars($_POST['cognome']);
-    $email = htmlspecialchars($_POST['email']);
-    $telefono = htmlspecialchars($_POST['telefono']);
-    $messaggio = htmlspecialchars($_POST['messaggio']);
+        // Imposta il contenuto dell'email
+        $subject = 'Nuovo messaggio di contatto';
+        $body = "Nome: $nome\nCognome o Ragione Sociale: $cognome\nEmail: $email\nTelefono: $telefono\nMessaggio:\n$messaggio";
 
-    // Imposta gli header dell'email
-    $to = 'info@finvideoholding.it';
-    $subject = 'Nuovo messaggio dal modulo di contatto';
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
-
-    // Imposta il contenuto dell'email
-    $body = "Nome: $nome\n\n";
-    $body .= "Cognome o Ragione Sociale: $cognome\n\n";
-    $body .= "Email: $email\n\n";
-    $body .= "Telefono: $telefono\n\n";
-    $body .= "Messaggio:\n$messaggio";
-
-    // Invia l'email
-    if (mail($to, $subject, $body, $headers)) {
-        // Redirect alla pagina principale dopo l'invio
-        header('Location: index.html'); // Cambia '/' con il percorso della tua pagina principale
-        exit();
+        // Invia l'email a info@finvideoholding.it
+        if (mail('info@finvideoholding.it', $subject, $body, $headers)) {
+            echo "Email inviata con successo.";
+            error_log("Email inviata a info@finvideoholding.it");
+        } else {
+            echo "Errore nell'invio dell'email.";
+            error_log("Errore nell'invio dell'email a info@finvideoholding.it");
+        }
     } else {
-        echo "<p>Errore nell'invio dell'email.</p>";
+        echo "Metodo di richiesta non valido.";
     }
-}
-?>
-
+    ?>
 </body>
 </html>
