4cb68c13f422d2f071c268bd0b0f951b58fe18b7
namazu-san <vppayassistenza@gmail.com>
2024-08-07 10:05:32 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+
+return [
+    'smtp_server' => 'smtp.paypoint.it',
+    'smtp_port' => 25,
+    'smtp_user' => 'biglietti@paypoint.it',
+    'smtp_pass' => '4cJ0d!*e23tJ&',
+];
+?>
